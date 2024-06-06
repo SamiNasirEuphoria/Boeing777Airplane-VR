496b0762ac6c2f8fe06695c8ed31b52811f74d75
sami nasir <sami.nasir@euphoria.pk>
2024-06-06 19:43:10 +0500

    Done Experience

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public LineRenderer myLine;
+    private void OnDisable()
+    {
+        if (myLine.startColor !=  myLine.endColor)
+        {
+            ObjectReferenceContainer.Instance.fadeEffect.SetTrigger("Teleport");
+        }
+    }
+}
